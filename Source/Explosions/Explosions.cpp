ddcf12ab1eae8083144d6c8e37d721bf13b19604
tuna <gtuna52@gmail.com>
2022-05-26 10:37:54 +0300

    initial commit

---
+++ Source/Explosions/Explosions.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Explosions.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Explosions, "Explosions" );
+ 
